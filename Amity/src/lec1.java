3355886c1b8f379d5cbfb7f814bdcbf6a12c99ec
shubh-soubhagya <soubhagyasrivastava240@gmail.com>
2025-06-24 11:27:03 +0530

    Day 1

---
+++ Amity/src/lec1.java
@@ -0,0 +1,35 @@
+package src;
+
+// public class lec1 {
+    
+//     public static int main(String[] args){
+//         return 0;
+//     }
+// }
+
+public class lec1 {
+    
+    public static void main(String[] args){
+
+        // int a =5;
+        // int b =4;
+        // int sum = a+b;
+        // System.out.println("Sum of numbers: "+sum);
+
+        // double speed=60;
+        //  if (speed>=100){System.out.println("Overspeed!");} 
+        // else if(speed>50 && speed <100){System.out.println("Average speed");}
+        // else{System.out.println("Normal Speed");}
+
+        int age = 18/2;
+
+        if(age<10){System.out.println("Cannot sit in front seat!");}
+        else if (age>=10 && age<16){System.out.println("Can seat in front seat, but cannot drive!");}
+        else if (age>=16 && age<18){System.out.println("Can opt for learning licesnce");}
+        else {System.out.println("Can drive");}
+
+        
+
+    
+    }
+}
