bdbc55a8af5ce0fa0f73c71a9d17440087659122
shubh-soubhagya <soubhagyasrivastava240@gmail.com>
2025-06-26 01:01:58 +0530

    christmas tree

--- Amity/day2/star_pattern.java
+++ Amity/day2/star_pattern.java
@@ -17,18 +17,44 @@ public class star_pattern {
 
 
         
-        Scanner sc = new Scanner(System.in);
-        int row = sc.nextInt();
-        int i=0;
+        // Scanner sc = new Scanner(System.in);
+        // int row = sc.nextInt();
+        // int i=0;
 
-        while(i<row){
-            int j=0;
-            while(j<=i){
-                System.out.print(" * ");
+        // while(i<row){
+        //     int j=0;
+        //     while(j<=i){
+        //         System.out.print(" * ");
+        //         j++;
+        //     }
+        //     System.out.println();
+        //     i++;
+
+
+        int i = 1;
+
+        while (i <= 5) {
+            // Add leading spaces
+            int spaces = 5 - i;  // change based on total rows (5 here)
+            int s = 0;
+            while (s < spaces) {
+                System.out.print("  ");  // double space for better alignment
+                s++;
+            }
+
+            // Star pattern (odd number using 2*i - 1)
+            int j = 0;
+            int m = 2 * i - 1;
+            while (j < m) {
+                System.out.print("* ");
                 j++;
             }
-            System.out.println();
+
+            System.out.println();  // go to next line
             i++;
+
+ 
         }
     }
 }
+
