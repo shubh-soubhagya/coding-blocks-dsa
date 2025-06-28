0076cc887876dd7fe4d3adca9be73351247736be
shubh-soubhagya <soubhagyasrivastava240@gmail.com>
2025-06-28 23:43:06 +0530

    -

--- Amity/day2/star_pattern1.java
+++ Amity/day2/star_pattern1.java
@@ -1,59 +1,7 @@
 package day2;
-import java.util.*;
 
 public class star_pattern1 {
-    public static void main(String[] args) {
-        // int i = 0;
-
-        // while(i <= 4){
-        //     int j = 0;
-        //     while(j <= 4){
-        //         System.out.print(" * ");
-        //         j++;
-        //     }
-        //     System.out.println();
-        //     i++;
-        // }
-
-
-        
-        // Scanner sc = new Scanner(System.in);
-        // int row = sc.nextInt();
-        // int i=0;
-
-        // while(i<row){
-        //     int j=0;
-        //     while(j<=i){
-        //         System.out.print(" * ");
-        //         j++;
-        //     }
-        //     System.out.println();
-        //     i++;
-
-
-        // int i = 1;
-
-        // while (i <= 5) {
-        //     // Add leading spaces
-        //     int spaces = 5 - i;  // change based on total rows (5 here)
-        //     int s = 0;
-        //     while (s < spaces) {
-        //         System.out.print("  ");  // double space for better alignment
-        //         s++;
-        //     }
-
-        //     // Star pattern (odd number using 2*i - 1)
-        //     int j = 0;
-        //     int m = 2 * i - 1;
-        //     while (j < m) {
-        //         System.out.print("* ");
-        //         j++;
-        //     }
-
-        //     System.out.println();  // go to next line
-        //     i++;
-        // }
-
+    public static void main(String[] args){
 
         int i = 4;
 
