a9d34779a5e124f3421a9dd603853f89f7e1d149
shubh-soubhagya <soubhagyasrivastava240@gmail.com>
2025-06-30 09:34:57 +0530

    Code of basic problems

---
+++ day3/PrimeCheck.java
@@ -0,0 +1,28 @@
+
+import java.util.Scanner;
+
+public class PrimeCheck {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter Number: ");
+        int num = sc.nextInt();
+
+        int i = 1;
+        int count = 0;
+
+        for(i=1; i<=num; i++){
+            if(num % i == 0){
+                count++;
+            }
+        }
+
+        if (count == 2){
+            System.out.println("prime");
+        } 
+         else {
+            System.out.println("not prime!");
+        }
+
+        sc.close();
+    }
+}
