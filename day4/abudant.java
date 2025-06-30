0efd81d956f89d91241a893a0b1c08cb12aad647
shubh-soubhagya <soubhagyasrivastava240@gmail.com>
2025-06-30 09:36:44 +0530

    conversion

---
+++ day4/abudant.java
@@ -0,0 +1,30 @@
+
+import java.util.Scanner;
+
+
+public class abudant {
+    public static void main(String[] args){
+
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter Your number: ");
+        int n = sc.nextInt();
+
+        int divisor = 0;
+        int sum =0;
+        for(int i=1; i<n; i++){
+            
+            if(n%i==0){
+                divisor=i;
+                sum =  sum + divisor;
+            }
+
+        }
+        System.out.println("Sum:" + sum);
+
+        // if(sum>n)System.out.println("Abudant number!");
+        // else if(sum==n)System.out.println("Neutral Number");
+        // else System.out.println("Deficit number");
+
+        System.out.println(n==sum?"Neutral":n>sum?"Deficet":"Abudant");
+    }
+}
---
+++ day4/celsius_conversion.java
@@ -0,0 +1,16 @@
+
+// import java.util.Scanner;
+
+public class celsius_conversion {
+    public static void main(String[] args){
+
+        for(int i = 20; i<=100; i++){
+            if(i%2==0){
+                double farhenheit = ((9.0/5)*(i))+32;
+                System.out.println(farhenheit);
+
+            }
+            
+        }
+    }
+}
