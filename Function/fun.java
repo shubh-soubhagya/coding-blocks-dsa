52e0bb87569ab9a7f1e0bcaa6e2402ce7cfffb08
shubh-soubhagya <soubhagyasrivastava240@gmail.com>
2025-06-30 09:56:42 +0530

    function basics

---
+++ Function/fun.java
@@ -0,0 +1,16 @@
+public class fun {
+    public static void main(String[] args){
+
+        fun1();
+        sum(1,2);
+
+    }
+
+    public static void fun1(){
+        System.out.println("Hello World");
+    }
+
+    public static void sum(int a, int b){
+        System.out.println(a+b);
+    }
+}
