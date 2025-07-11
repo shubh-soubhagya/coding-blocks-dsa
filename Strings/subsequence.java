ae037eec23c772b5dced7107f66bb3f65014e712
shubh-soubhagya <soubhagyasrivastava240@gmail.com>
2025-07-11 15:02:36 +0530

    strings

---
+++ Strings/subsequence.java
@@ -0,0 +1,16 @@
+public class subsequence {
+    public static void main(String[] args) {
+        String s ="abc";
+        subseq(s,0,"");
+    }
+    public static void subseq(String s, int i, String ans){
+        if(i==s.length()){
+            System.out.println(ans);
+            return;
+        }
+        //include
+        subseq(s,i+1,ans+s.charAt(i));
+        //exclude
+        subseq(s,i+1,ans);
+    }
+}
